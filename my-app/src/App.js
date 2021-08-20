1526d883fda508fe9e406face1f38a78cd9948b3
Kieran Volz <kieran.volz@gmail.com>
2021-08-20 13:40:10 -0400

    updated based on codepen
    took just about everything from original codepen and put it into repo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { Component } from 'react';
 
 
 import { FaEllipsisH, FaSearch } from 'react-icons/fa';
 
+const element = <FaSearch className="titanium-white" />;
 function App() {
   return (
-    <div className="App">
+    <div>
+      <div className="dark-blue">
+        <h2 className="titanium-white">ToDo's</h2>
+        <FaEllipsisH           className="titanium-white main-menu" />
 
-<FaEllipsisH /> <FaSearch />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="tabs">
+          <Tabs>
+            <Tab label="Open">
+            </Tab>
+            <Tab label="Closed">
+            </Tab>
+          </Tabs>
+        </div>
+      </div>
+
+      <div className="darker-blue">
+        <form>
+          <label>
+            <input type="text" name="name" placeholder="Search Todo's" />
+            <FaSearch 
+                          id="search-icon"
+                          className="titanium-white"
+            
+            />
+          </label>
+        </form>
+        <div className="dark-blue-underline"></div>
+      </div>
     </div>
   );
 }
+
+class Tabs extends React.Component {
+  state = {
+    activeTab: this.props.children[0].props.label
+  };
+  changeTab = (tab) => {
+    this.setState({ activeTab: tab });
+  };
+  render() {
+    let content;
+    let buttons = [];
+    return (
+      <div>
+        {React.Children.map(this.props.children, (child) => {
+          buttons.push(child.props.label);
+          if (child.props.label === this.state.activeTab)
+            content = child.props.children;
+        })}
+
+        <TabButtons
+          activeTab={this.state.activeTab}
+          buttons={buttons}
+          changeTab={this.changeTab}
+        />
+        <div className="tab-content">{content}</div>
+      </div>
+    );
+  }
+}
+
+const TabButtons = ({ buttons, changeTab, activeTab }) => {
+  return (
+    <div className="tab-buttons">
+      {buttons.map((button) => {
+        return (
+          <button
+            className={button === activeTab ? "active" : ""}
+            onClick={() => changeTab(button)}
+          >
+            {button}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+const Tab = (props) => {
+  return <React.Fragment>{props.children}</React.Fragment>;
+};
+
+
 
 export default App;
