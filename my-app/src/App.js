df69bda3cc92d3637697040f16919a94067262f6
Kieran Volz <kieran.volz@gmail.com>
2022-01-29 11:04:25 -0500

    fixed dropdown menu
    dropdown menu looks ok
    
    still need to fix position
    
    still need to fix link colors when clicked and hovered over

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,6 @@
-import './App.css';
-import React  from 'react';
-
-
-import { FaEllipsisH, FaSearch, FaCheck} from 'react-icons/fa';
-
-
+import "./App.css";
+import React from "react";
+import { FaEllipsisH, FaSearch, FaCheck } from "react-icons/fa";
 function App() {
   return (
     <div>
@@ -12,121 +8,175 @@ function App() {
         <h1 className="titanium-white">ToDo's</h1>
         <FaEllipsisH className="titanium-white main-menu" />
 
-
-
-
-
-
-
-
         <div className="tabs">
           <Tabs>
-            <Tab  label="Open">
-            </Tab>
-            <Tab  label="Closed">
-            </Tab>
+            <Tab label="Open"></Tab>
+            <Tab label="Closed"></Tab>
           </Tabs>
         </div>
         <div className="gradient-above-search"></div>
-
       </div>
 
       <div className="darker-blue">
-<div className="center">
-        <form>
-          <label>
-            <input type="text" name="name" placeholder="Search Todo's" />
-            <FaSearch id="search-icon" className="titanium-white" />
-          </label>
-        </form>
+        <div className="center">
+          <form>
+            <label>
+              <input type="text" name="name" placeholder="Search Todo's" />
+              <FaSearch id="search-icon" className="titanium-white" />
+            </label>
+          </form>
         </div>
         <div className="dark-blue-underline"></div>
-     
-<div className="scrollable">
-<div className="scroller-top-gradient"></div>
 
-        {/* you got some work to do below */}
-        <div className="grid">
+        <div className="scrollable">
+          <div className="scroller-top-gradient"></div>
 
-        <div className="to-do-card">
-        <div className="checked-circle"></div>
+          {/* you got some work to do below */}
+          <div className="grid">
+            <div className="to-do-card">
+              <div className="checked-circle"></div>
 
-        {/* testing checkbox idea */}
+              {/* testing checkbox idea */}
 
-<div class="customcb">
-  <input type="checkbox" value="1" id="customcb" name="" />
- 
+              <div class="customcb">
+                <input type="checkbox" value="1" id="customcb" name="" />
 
-  <label class="inner" for="customcb">
+                <label class="inner" for="customcb">
+                  <FaCheck className="check-icon" value="1" />
+                </label>
+              </div>
 
-  <FaCheck className='check-icon' value="1"/>
+              {/* testing checkbox idea */}
 
+              <div className="todo-text titanium-white">UI Design</div>
 
-  </label>
+              {/* <div className="username-box">User: 1</div> */}
 
-
-
- </div>
-
-
-
-
-        {/* testing checkbox idea */}
-
-
-          <div className="todo-text titanium-white">UI Design</div>
-          
-          {/* <div className="username-box">User: 1</div> */}
-            
-          <div className="to-do-menu  ">
-            
-
-<ul>
-            <li>
-            <a href="#"><FaEllipsisH className='titanium-white'/></a>
-            <ul>
-                <li className='tooltip_link'><a href="#" >Edit ToDo</a></li>
-                <li className='tooltip_link'><a href="#" >Mark Completed</a></li>
-            </ul>
-        </li>
-    </ul>
-
+              <div className="to-do-menu  ">
+                <ul>
+                  <li>
+                    <a href="#">
+                      <FaEllipsisH className="titanium-white" />
+                    </a>
+                    <ul>
+                      <li className="tooltip_link">
+                        <a href="#">Edit ToDo</a>
+                      </li>
+                      <li className="tooltip_link">
+                        <a href="#">Mark Completed</a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
             </div>
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
             </div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-            <div className="to-do-card"><div className="to-do-menu titanium-white"><FaEllipsisH /></div></div>
-       
-
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
+            </div>
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
+            </div>
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
+            </div>
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
+            </div>
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
+            </div>
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
+            </div>
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
+            </div>
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
+            </div>
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
+            </div>
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
+            </div>
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
+            </div>
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
+            </div>
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
+            </div>
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
+            </div>
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
+            </div>
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
+            </div>
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
+            </div>
+            <div className="to-do-card">
+              <div className="to-do-menu titanium-white">
+                <FaEllipsisH />
+              </div>
+            </div>
+          </div>
+          {/* end of grid */}
         </div>
-        {/* end of grid */}
-        </div>
-
       </div>
     </div>
   );
 }
 
-class Tabs  extends React.Component {
-// class Tabs  extends React {
+class Tabs extends React.Component {
+  // class Tabs  extends React {
   state = {
-    activeTab: this.props.children[0].props.label
+    activeTab: this.props.children[0].props.label,
   };
   changeTab = (tab) => {
     this.setState({ activeTab: tab });
@@ -174,6 +224,13 @@ const Tab = (props) => {
   return <React.Fragment>{props.children}</React.Fragment>;
 };
 
-
-
 export default App;
+
+
+
+
+{/* check out this link for fixing the dropdown menu
+
+  https://codepen.io/ig_design/pen/MWKVrNR?editors=1100
+
+*/}
